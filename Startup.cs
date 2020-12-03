61a955b9b763b9fe924e5ac31510e7fb2f0499e6
Visar Sylejmani <sylejmani.visar.97@gmail.com>
2020-12-03 21:54:33 +0100

    Suppresion du package Microsoft.AspNetCore.Mvc.NewtonsoftJson

--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,6 @@ namespace USMBAPI
         {
             services.AddControllers();
 
-            services.AddControllers().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
